602eb4d21077dae57fe3db18ea2787f7f9c7ea8e
Kate <82730879+keumul@users.noreply.github.com>
2021-09-20 22:01:43 +0300

    Add files via upload

---
+++ Lab14/1.cpp
@@ -0,0 +1,82 @@
+﻿// В соответствии со своим вариантом построить хеш - таблицы разного размера(например, 16, 32 или 32, 64, 128) с коллизиями.
+// Исследовать время поиска в хеш - таблицах. В приложении Excel построить соответствующие графики.
+// Реализовать динамическую хеш-таблицу с открытой адресацией для хранения строк. Таблица должна увеличивать свой размер вдвое при достижении
+// 50% заполнения.
+
+//#include "stdafx.h"
+#include "Hash.h"
+#include <iostream>
+using namespace std;
+struct AAA //основная хэшевая структура
+{
+	int key;
+	const char* mas;
+	AAA(int k, const char* z)
+	{
+		key = k;  mas = z;
+
+	} AAA() {}
+};
+//-------------------------------
+int key(void* d) 
+{
+	AAA* f = (AAA*)d;   
+	return f->key;
+}
+//-------------------------------
+void AAA_print(void* d)
+{
+	cout << " ключ " << ((AAA*)d)->key << " - " << ((AAA*)d)->mas << endl;
+}
+//-------------------------------
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	AAA a1(1, "one"), a2(2, "two"), a3(4, "three"), a4(2, "four");
+	int siz = 10, choice, k, siz2=0, siz3=0;
+	cout << "Введите размер хеш-таблицы" << endl; 	cin >> siz;
+	siz2 = siz / 2;
+	siz3 = 2 * siz;
+	Object H = create(siz, key); 
+	for (;;)
+	{
+		cout << "1 - вывод хеш-таблицы" << endl;
+		cout << "2 - добавление элемента" << endl;
+		cout << "3 - удаление элемента" << endl;
+		cout << "4 - поиск элемента" << endl;
+		cout << "0 - выход" << endl;
+		cout << "сделайте выбор" << endl; 
+		cin >> choice;
+		switch (choice)
+		{
+		case 0:  exit(0);
+		case 1: H.scan(AAA_print);  break;
+		case 2: { AAA* a = new AAA;
+			char* str = new char[20];
+			cout << "введите ключ" << endl;	cin >> k;
+			a->key = k;
+			cout << "введите строку" << endl; cin >> str;
+			a->mas = str;
+
+			int count = 0;
+			
+			if (H.N == H.size)
+				cout << "Таблица заполнена" << endl;
+			else
+				H.insert(a);
+		} break;
+		case 3: {  cout << "введите ключ для удаления" << endl;
+			cin >> k;
+			H.deleteByKey(k);
+		}  break;
+		case 4: {  cout << "введите ключ для поиска" << endl;
+			cin >> k;
+			if (H.search(k) == NULL)
+				cout << "Элемент не найден" << endl;
+			else
+				AAA_print(H.search(k));
+		}  break;
+		}
+	}
+	return 0;
+}
---
+++ Lab14/hash.cpp
@@ -0,0 +1,148 @@
+﻿//#include "stdafx.h"
+#include "Hash.h"
+#include <iostream>
+
+//int HashFunction(int key, int size, int p)//Хеш-функция
+//{
+//	double key2 = 5 * ((0.6180339887499 * key) - int((0.6180339887499 * key)));
+//	return (p + key) % size;
+//}
+
+//---------------------------МОДУЛЬНОЕ ХЭШИРОВАНИЕ-----------------------------------//
+
+//int HashFunction(int key, int size, int p)//Хеш-функция
+//{
+//	return key % 5;
+//}
+
+//---------------------------МУЛЬТИПЛИКАТИВНОЕ ХЭШИРОВАНИЕ---------------------------//
+//int HashFunction(int key, int size, int p) //Хеш-функция
+//{
+//	int n = 13; double A = 0.6180339887499;
+//	int h = n * fmod(key * A, 1) +p;
+//	return h;
+//}
+
+
+//----Изменить функцию вычисления хеш для решения коллизии на линейную функцию,  которая--// 
+//----строится на основе формулы: h(key, i) = (h'(key) + i)×mod  hashTableSize.-----------//
+
+//inline int HashFunction(int key, int size, int p) //Хеш-функция
+//{
+//	int h = fmod(key + 13, size) + p;
+//	return h;
+//}
+
+//------Изменить функцию вычисления хеш для решения коллизии на функцию, которая----------//
+//------строится на основе формулы : h(key, i) = (h1(key) + i∙h2(key))×mod hashTableSize,-//
+//------где  h1(key) = key×mod hashTableSize, h2(key) = 1 + (key×mod hashTableSize).------//
+
+int HashFunction(int key, int size, int p) //Хеш-функция
+{
+	int n = 13; double A = 0.6180339887499;
+	int h1 = fmod(key, size) + p;
+	int h2 = 1 + fmod(key, size) + p;
+	int h = fmod(h1+n*h2,size) + p;
+	return h;
+}
+
+//-------------------------------
+int Next_hash(int hash, int size, int p)
+{
+	//1ый метод
+	//return (hash + 5 * p + 3 * p * p) % size;
+	
+	//модульный метод
+	//return hash % 5;
+	
+	//мультипликативный метод
+	//return 13 * fmod(hash*0.6180339887499, 1) + p;
+
+	//2й доп
+	//return fmod(hash + 13, size) + p;
+
+	//3й доп
+	return fmod(fmod(hash, size) + 13 * (1 + fmod(hash, size)), size) + p;
+}
+//-------------------------------
+Object create(int size, int(*getkey)(void*))
+{
+	return *(new Object(size, getkey));
+}
+//-------------------------------
+Object::Object(int size, int(*getkey)(void*))
+{
+	N = 0;
+	this->size = size;
+	this->getKey = getkey;
+	this->data = new void* [size];
+	for (int i = 0; i < size; ++i)
+		data[i] = NULL;
+}
+//-------------------------------
+bool Object::insert(void* d)
+{
+	bool b = false;
+	if (N != size)
+		for (int i = 0, t = getKey(d), j = HashFunction(t, size, 0);
+			i != size && !b;  j = Next_hash(j, size, ++i))
+			if (data[j] == NULL || data[j] == DEL)
+			{
+				data[j] = d;
+				N++;
+				b = true;
+			}
+	return b;
+}
+//-------------------------------
+int Object::searchInd(int key)
+{
+	int t = -1;
+	bool b = false;
+	if (N != 0)
+		for (int i = 0, j = HashFunction(key, size, 0); data[j] != NULL && i != size && !b; j = HashFunction(key, size, ++i))
+			if (data[j] != DEL)
+				if (getKey(data[j]) == key)
+				{
+					t = j; b = true;
+				}
+	return t;
+}
+//-------------------------------
+void* Object::search(int key)
+{
+	int t = searchInd(key);
+	return(t >= 0) ? (data[t]) : (NULL);
+}
+//-------------------------------
+void* Object::deleteByKey(int key)
+{
+	int i = searchInd(key);
+	void* t = data[i];
+	if (t != NULL)
+	{
+		data[i] = DEL;
+		N--;
+	}
+	return t;
+}
+//-------------------------------
+bool Object::deleteByValue(void* d)
+{
+	return(deleteByKey(getKey(d)) != NULL);
+}
+//-------------------------------
+void Object::scan(void(*f)(void*))
+{
+	for (int i = 0; i < this->size; i++)
+	{
+		std::cout << " Элемент" << i;
+		if ((this->data)[i] == NULL)
+			std::cout << "  пусто" << std::endl;
+		else
+			if ((this->data)[i] == DEL)
+				std::cout << "  удален" << std::endl;
+			else
+				f((this->data)[i]);
+	}
+}
